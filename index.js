13348852481fcda4f272d015e483af276ab2035e
abcbakery <gywls5545@naver.com>
2023-11-07 14:21:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -190,6 +190,7 @@ function ladder(_id, option = {}){
             let x1 = event.clientX - canvas.parentElement.offsetLeft || canvas.offsetLeft
             let y1 = event.clientY - canvas.parentElement.offsetTop || canvas.offsetTop    
             let endBridge =  _isInSide(x1,y1)  //가장 마지막의 선 지점값을 가져 옵니다
+            ctx.fillText('('+x1+','+y1+')', x1+10, y1+10)
             endBridge = {...endBridge, x: endBridge.object.x, y: y1}  //x축은 그려진 선 기준값을 대입 합니다
 
             //같은 선분 또는 옆 영역을 뛰어넘어가는 경우 등록하지 않습니다
