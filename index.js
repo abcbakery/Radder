6d0cca2470b1d65df33e63703972849917840e07
abcbakery <gywls5545@naver.com>
2023-11-09 19:42:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -86,6 +86,7 @@ function ladder(_id, option = {}){
             ctx.strokeStyle = defaultLineColor
             ctx.moveTo(startPosX, height * 0.1)
             ctx.lineTo(startPosX, height * 0.9)
+            ctx.fillText(Math.trunc(startPosX), startPosX+10, height * 0.1)
             ctx.stroke()
             ctx.closePath()
             ctx.restore()  
